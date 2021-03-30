321bad78f3b4509081bc333daee01a8f5e2bfa31
shreya <shreyarai465@gmail.com>
2021-03-30 19:00:50 +0530

    Integration with word buddy, done!

--- app/src/main/java/com/example/loginapplication/Second_activity.java
+++ app/src/main/java/com/example/loginapplication/Second_activity.java
@@ -1,14 +1,141 @@
 package com.example.loginapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Second_activity extends AppCompatActivity {
+    String url;
+    String url2;
+    private TextView showDef;
+    private TextView synonym;
+    private EditText enterWord;
+    Button btn_speechText;
+    Button findSyn;
+    TextToSpeech toSpeech;
+    ImageButton imgButton_speech;
+    ImageButton pronounce;
+    TextToSpeech pronounciation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second_activity);
+        showDef = findViewById(R.id.showDef);
+
+        synonym = findViewById(R.id.synonym);
+        synonym.setVisibility(View.INVISIBLE);
+        showDef.setVisibility(View.INVISIBLE);
+        enterWord = findViewById(R.id.enterWord);
+//        btn_speechText = (Button) findViewById(R.id.btn_speechText);
+        imgButton_speech = (ImageButton) findViewById(R.id.imgButton_speech);
+        pronounce = (ImageButton) findViewById(R.id.pronounce);
+        toSpeech =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i!=TextToSpeech.ERROR)
+                    toSpeech.setLanguage(Locale.ENGLISH);
+            }
+        });
+        pronounciation = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i!=TextToSpeech.ERROR)
+                    pronounciation.setLanguage(Locale.ENGLISH);
+            }
+        });
+
+        pronounce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pronounciation.speak(enterWord.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+
+/*       btn_speechText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toSpeech.speak(showDef.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        }); */
     }
+    private String dictionaryEntries() {
+        final String language = "en-gb";
+        final String word = enterWord.getText().toString();
+        final String fields = "definitions";
+        final String strictMatch = "false";
+        final String word_id = word.toLowerCase();
+        return "https://od-api.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id + "?" + "fields=" + fields + "&strictMatch=" + strictMatch;
+    }
+
+    private String dictionaryEntries2() {
+        final String language2 = "en";
+        final String word2 = enterWord.getText().toString();
+        final String fields2 = "synonyms";
+        final String strictMatch2 = "false";
+        final String word_id2 = word2.toLowerCase();
+        return "https://od-api.oxforddictionaries.com/api/v2/thesaurus/" + language2 + "/" + word_id2 + "?" + "fields=" + fields2 + "&strictMatch=" + strictMatch2;
+    }
+    public void sendRequestOnClick(View v)
+    {
+        DictionaryRequest dR = new DictionaryRequest(this, showDef);
+        showDef.setVisibility(View.VISIBLE);
+
+        url = dictionaryEntries();
+
+        dR.execute(url);
+
+    }
+    public void sendRequestOnClick2(View v)
+    {
+        ThesaurusRequest dR2 = new ThesaurusRequest(this, synonym);
+        synonym.setVisibility(View.VISIBLE);
+        url2 = dictionaryEntries2();
+        dR2.execute(url2);
+    }
+
+
+    public void btnSpeech(View view) {
+        Intent intent=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi Speak Something");
+        try {
+            startActivityForResult(intent,1);
+        }catch(ActivityNotFoundException e){
+            Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode)
+        {
+            case 1:
+                if(resultCode==RESULT_OK && null!=data){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    enterWord.setText(result.get(0));
+                }
+                break;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/loginapplication/ThesaurusRequest.java
@@ -0,0 +1,113 @@
+package com.example.loginapplication;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
+
+//in android calling network requests on the main thread forbidden by default
+//create class to do async job
+public class ThesaurusRequest extends AsyncTask<String, Integer, String> {
+    Context context2;
+    TextView synonym;
+
+    ThesaurusRequest(Context context2, TextView tV)
+    {
+        this.context2 = context2;
+        synonym = tV;
+    }
+    @Override
+    protected String doInBackground(String... params) {
+
+        //TODO: replace with your own app id and app key
+        final String app_id = "c2e7b1fe";
+        final String app_key = "c36dcd311d964bfd7041f090522ec733";
+        try {
+            URL url = new URL(params[0]);
+            HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Accept","application/json");
+            urlConnection.setRequestProperty("app_id",app_id);
+            urlConnection.setRequestProperty("app_key",app_key);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+
+            // read the output from the server
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+
+            return stringBuilder.toString();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        try {
+            String Syn = "";
+            JSONObject js = new JSONObject(result);
+            JSONArray results = js.getJSONArray("results");
+            JSONObject lEntries = results.getJSONObject(0);
+            JSONArray laArray = lEntries.getJSONArray("lexicalEntries");
+            JSONObject entries = laArray.getJSONObject(0);
+            JSONArray e = entries.getJSONArray("entries");
+            JSONObject jsonObject = e.getJSONObject(0);
+            JSONArray sensesArray = jsonObject.getJSONArray("senses");
+
+            JSONObject de = sensesArray.getJSONObject(0);
+
+            JSONArray d = de.getJSONArray("subsenses");
+
+            JSONObject sub = sensesArray.getJSONObject(0);
+
+            JSONArray synArr = de.getJSONArray("synonyms");
+            String[] synonyms = new String[100];
+            List<String> allNames = new ArrayList<String>();
+            for (int i = 0, size = 5; i < size; i++) {
+                JSONObject objectInArray = synArr.getJSONObject(i);
+                String s = objectInArray.getString("text");
+                allNames.add(s);
+
+
+                synonyms[i] = s;
+            }
+            synonym.setText(allNames.toString());
+            // synonym.setText(synonyms[0].toString()+", "+synonyms[1].toString());
+
+            Log.d("synonyms are ","hi" +synonyms.toString());
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.v("Result of Dictionary", "onPostExecute" +result);
+
+        System.out.println(result);
+
+
+    }
+}
